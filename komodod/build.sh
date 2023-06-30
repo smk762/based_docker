18d35d0448eb7844987163ebc7412201221405d5
smk762 <smk762@iinet.net.au>
2023-07-01 01:43:51 +0800

    tweak build

--- komodod/build.sh
+++ komodod/build.sh
@@ -3,6 +3,7 @@ set -euxo pipefail
 echo $PWD
 echo "========================================"
 git clone https://github.com/KomodoPlatform/komodo
-cd komodo && git checkout 156dba6
-
+cd komodo && git checkout 156dba6 && \
+./autogen.sh && \
+./configure --with-incompatible-bdb --with-gui || true && \
 ./zcutil/build.sh -j$(nproc -1)
